09fc1f5985c6a6296b1b08e982f7d20f0604477c
Stainley Lebron <stainley.lebron@gmail.com>
2023-09-03 21:49:21 -0400

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Stainley A Lebron R on 2023-09-03.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
